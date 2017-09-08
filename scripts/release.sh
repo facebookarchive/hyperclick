7fe3feb8f23ffbe84f045aeaf0a7505744b5e73c
hansonw <hansonw@fb.com>
2017-09-08 13:21:16 -0700

    Include sourcemaps

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 node_modules/.bin/webpack --config webpack.prod.js
-rm -rf keymaps styles scripts webpack*
+rm -rf keymaps styles scripts webpack* yarn.lock
 cp -r node_modules/atom-ide-ui/pkg/hyperclick/{keymaps,styles} .
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
     filename: 'index.js',
     libraryTarget: 'commonjs2',
   },
+  devtool: 'source-map',
   externals: [
     'atom',
     'electron',
